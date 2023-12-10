089e05087043008c23044eaa625ebb24da90907c
Gonçalo Abreu Corrêa Brito da Silva <progoncas2012@gmail.com>
2023-12-10 22:57:28 +0000

    feat: update modules

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,13 @@
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { AccountModule } from './account/account.module';
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { AccountModule } from "./account/account.module";
+import { ClientModule } from './client/client.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/mongo'),
-    AccountModule
+    MongooseModule.forRoot("mongodb://localhost:27017/mongo"),
+    AccountModule,
+    ClientModule,
   ],
 })
 export class AppModule {}

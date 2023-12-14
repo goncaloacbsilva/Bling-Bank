b58824df5ee7c7798b317affc91d02626eb58148
Gonçalo Abreu Corrêa Brito da Silva <progoncas2012@gmail.com>
2023-12-14 19:57:21 +0000

    use .env

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,8 @@
-import { Module } from "@nestjs/common";
+import { Logger, Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { AccountModule } from "./account/account.module";
 import { ClientModule } from "./client/client.module";
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import { PaymentModule } from "./payment/payment.module";
 import { ScheduleModule } from "@nestjs/schedule";
 import { CacheModule } from "@nestjs/cache-manager";
@@ -18,7 +18,24 @@ import { Duration } from "luxon";
         days: 15,
       }).as("milliseconds"),
     }),
-    MongooseModule.forRoot("mongodb://localhost:27017/mongo"),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const logger = new Logger("MongooseModule");
+
+        const uri = configService.getOrThrow("DB_CONNECTION");
+        const useTLS = configService.getOrThrow("DB_USE_TLS") == "true";
+
+        logger.log(`Connecting to ${uri}...`);
+        if (useTLS) logger.warn(`TLS enabled`);
+
+        return {
+          uri: uri,
+          tls: useTLS,
+        };
+      },
+      inject: [ConfigService],
+    }),
     AccountModule,
     ClientModule,
     PaymentModule,

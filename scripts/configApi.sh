1f84fabb943019a85860991e338fe16b5b76df90
Renato Miguel Monteirinho Custódio <renato.miguel.monteirinho.custodio@tecnico.ulisboa.pt>
2023-12-15 01:28:12 +0000

    Updated Scripts

--- scripts/configApi.sh
+++ scripts/configApi.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo apt install ifupdown
-sudo ifconfig eth0 192.168.0.100/24 up
-sudo ip route add default via 192.168.0.10
+sudo ifconfig eth0 192.168.0.2/24 up
+sudo ifconfig eth1 192.168.2.1/24 up
+sudo ip route add default via 192.168.0.2
 sudo systemctl restart NetworkManager
--- scripts/configCli.sh
+++ scripts/configCli.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 sudo apt install ifupdown
-sudo ifconfig eth0 192.168.1.1/24 up
-sudo ip route add default via 192.168.1.254
-sudo systemctl restart NetworkManager
+sudo ifconfig eth0 192.168.1.2/24 up
+sudo sed -i "/nameserver/d" /etc/resolv.conf
+echo "nameserver 8.8.8.8" | sudo tee -a /etc/resolv.conf
+
+

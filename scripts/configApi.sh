73331c51b5db67cac0c3635b2fdc027f39bc54ae
Gonçalo Abreu Corrêa Brito da Silva <progoncas2012@gmail.com>
2023-12-16 20:05:57 +0000

    fix: wrong default gateway

--- scripts/configApi.sh
+++ scripts/configApi.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-sudo apt install ifupdown
 sudo ifconfig eth0 192.168.0.2/24 up
 sudo ifconfig eth1 192.168.2.1/24 up
-sudo ip route add default via 192.168.0.2
+sudo ip route add default via 192.168.0.1
 sudo systemctl restart NetworkManager

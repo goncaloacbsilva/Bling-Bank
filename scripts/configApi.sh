3412537a16b0488a9579d047569a75ebda5103a0
Gonçalo Abreu Corrêa Brito da Silva <progoncas2012@gmail.com>
2023-12-16 20:15:19 +0000

    fix: remove NetworkService restart

--- scripts/configApi.sh
+++ scripts/configApi.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+# Setup routes
+
+# DMZ
 sudo ifconfig eth0 192.168.0.2/24 up
+# Database
 sudo ifconfig eth1 192.168.2.1/24 up
+# Default Gateway (Router)
 sudo ip route add default via 192.168.0.1
-sudo systemctl restart NetworkManager
+
+# Setup DNS
+
+# Clear nameserver entries
+sudo sed -i "/nameserver/d" /etc/resolv.conf
+
+# Add 8.8.8.8 entry
+echo "nameserver 8.8.8.8" | sudo tee -a /etc/resolv.conf

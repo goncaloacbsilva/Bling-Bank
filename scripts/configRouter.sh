028872132344055dfe5e9fedaf6f9281208275fb
Gonçalo Abreu Corrêa Brito da Silva <progoncas2012@gmail.com>
2023-12-17 15:58:33 +0000

    config: create rules

--- scripts/configRouter.sh
+++ scripts/configRouter.sh
@@ -1,33 +1,21 @@
 #!/bin/bash
 
+echo "Configuring interfaces..."
 sudo ifconfig eth0 192.168.0.1/24 up
-
 sudo ifconfig eth1 192.168.1.1/24 up
-
 sudo sysctl net.ipv4.ip_forward=1
 
-sudo sed -i "/nameserver/d" /etc/resolv.conf
+# Configure DNS
 
+echo "Configuring DNS..."
+sudo sed -i "/nameserver/d" /etc/resolv.conf
 echo "nameserver 8.8.8.8" | sudo tee -a /etc/resolv.conf
 
-sudo iptables -F
+# Apply iptables config
 
-sudo iptables -F -t nat
+echo "Configuring firewall rules..."
+sudo cp rules/router.v4 /etc/iptables/rules.v4
+sudo cp rules/router.v6 /etc/iptables/rules.v6
 
-sudo iptables -t nat -A PREROUTING -i eth1 -p tcp --dport 80 -j DNAT --to-destination 192.168.0.2
-
-sudo iptables -t nat -A POSTROUTING  -o eth2 -j MASQUERADE
-
-sudo iptables -t nat -A POSTROUTING -o eth0 -p tcp --dport 80 -d 192.168.0.2 -j SNAT --to-source 192.168.0.1
-
-sudo iptables -A FORWARD -i eth1 -o eth0 -p tcp --syn --dport 80 -m conntrack --ctstate NEW -j ACCEPT
-
-sudo iptables -A FORWARD -i eth1 -o eth0 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-
-sudo iptables -A FORWARD -i eth0 -o eth1 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-
-sudo iptables -P FORWARD DROP
-
-#sudo iptables -P INPUT DROP
-
-#sudo iptables -P OUTPUT DROP
+echo "Reloading netfilter service..."
+sudo service netfilter-persistent reload
